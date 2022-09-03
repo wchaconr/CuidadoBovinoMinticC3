228ca247e78d6d2d3e7f1a51ff4eca471b573cf0
Wilder Chacón <will.rojas001@gmail.com>
2022-09-02 21:26:12 -0500

    Sprint 1

---
+++ bovino.dominio/Entidades/Aper_propietario.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bovino.dominio
+{
+    public class Aper_propietario: Persona
+    {
+        public int Id_Propietario{get;set;}
+        public int Nombre_Hacienda{get;set;}
+
+    }
+}
---
+++ bovino.dominio/Entidades/Aper_veterinario.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bovino.dominio
+{
+    public class Aper_veterinario: Persona
+    {
+        public int Id_Veterinario{get;set;}
+        public int Tarjeta_Profesional{get;set;}
+
+    }
+}
